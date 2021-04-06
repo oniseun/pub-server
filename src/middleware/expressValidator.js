05f81b13cc9531074d9df69019154676c8710644
Victor <voni@getcarbon.co>
2021-04-06 01:16:50 +0100

    correct typo

--- src/middleware/expressValidator.js
+++ src/middleware/expressValidator.js
@@ -1,4 +1,4 @@
-const VelidationError = require('../models/ValidationError')
+const ValidationError = require('../models/ValidationError')
 const { StatusCodes } = require('http-status-codes')
 
 module.exports = function expressValidator(err,req, res, next) {  
@@ -6,7 +6,7 @@ module.exports = function expressValidator(err,req, res, next) {
     if (err && err.error && err.error.isJoi) {
         console.log('validation error::',err)
         // we had a joi error, let's return a custom 400 json response
-        return res.status(StatusCodes.BAD_REQUEST).json( new VelidationError(err.error.toString(), [err]));
+        return res.status(StatusCodes.BAD_REQUEST).json( new ValidationError(err.error.toString(), [err]));
 
       } else {
         // pass on to another error handler
